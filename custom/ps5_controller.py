4f72fce6b6ee707a00ab1c13479d7d8d3ba4baf9
crizzy9 <shyampadia@live.com>
2023-12-05 02:56:54 -0800

    small update to controller

--- custom/ps5_controller.py
+++ custom/ps5_controller.py
@@ -1,23 +1,22 @@
 from pydualsense import *
 
 def cross_down(state):
+    dualsense.light.setColorI(255,0,0) # set touchpad color to red
     print(f'cross {state}')
 
 
 def circle_down(state):
+    dualsense.light.setColorI(0,255,0) # set touchpad color to green
     print(f'circle {state}')
 
 
 def dpad_down(state):
+    dualsense.light.setColorI(0,0,255) # set touchpad color to blue
     print(f'dpad {state}')
 
 
 def joystick(stateX, stateY):
     print(f'lj {stateX} {stateY}')
-
-
-def gyro_changed(pitch, yaw, roll):
-    print(f'{pitch}, {yaw}, {roll}')
 
 # create dualsense
 dualsense = pydualsense()
@@ -29,11 +28,18 @@ dualsense.cross_pressed += cross_down
 dualsense.circle_pressed += circle_down
 dualsense.dpad_down += dpad_down
 dualsense.left_joystick_changed += joystick
-# dualsense.gyro_changed += gyro_changed
+
+if dualsense.state.L2:
+    # set L2 to rigid mode and set force to 100% when pressed
+    dualsense.triggerL.setMode(TriggerModes.Rigid)
+    dualsense.triggerL.setForce(1, 255)
 
 # read controller state until R1 is pressed
 while not dualsense.state.R1:
     ...
+
+# reset controller to default state
+dualsense.init()
 
 # close device
 dualsense.close()
